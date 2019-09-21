6190cbb1d468416fe4e412c4083c55806b7e912c
Adrian Júnior <dev.adrianjunior@gmail.com>
2019-09-20 22:15:22 -0300

    firebase added

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,9 +1,422 @@
-import React from './node_modules/react';
+import app from 'firebase/app';
+import 'firebase/auth';
 
-const Firebase = () => (
-  <div>
-    <h1>Firebase</h1>
-  </div>
-);
+const firebaseConfig = {
+  apiKey: variables.env.REACT_APP_API_KEY,
+  authDomain: variables.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: variables.env.REACT_APP_DATABASE_URL,
+  projectId: variables.env.REACT_APP_PROJECT_ID,
+  storageBucket: variables.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: variables.env.REACT_APP_MESSAGING_SENDER_ID,
+}
+
+class Firebase {
+  constructor() {
+    app.initializeApp(firebaseConfig);
+
+    this.auth = app.auth();
+  }
+
+  signUp = (email, password) => {
+    this.auth.createUserWithEmailAndPassword(email, password)
+  }
+
+  signIn = (email, password) => {
+      this.auth.signInWithEmailAndPassword(email, password)
+  }
+
+  signOut = () => {
+      this.auth.signOut();
+  }
+
+  resetPassword = email => {
+      this.auth.sendPasswordResetEmail(email);
+  }
+
+  updatePassword = password => {
+      this.auth.currentUser.updatePassword(password);
+  }
+
+  isLogged = () => {
+      this.auth.onAuthStateChanged(user => {
+          if(user) {
+              return true
+          } else {
+              return false
+          }
+      })
+  }
+
+  /* OFFER */
+
+  addLesson = lesson => {
+
+  } 
+
+  addDynamicLesson = lesson => {
+
+  } 
+
+  deleteLesson = lessonId => {
+
+  }
+
+  deleteDynamicLesson = lessonId => {
+
+  }
+
+  getLesson = lessonId => {
+
+  }
+
+  getLessons = () => {
+
+  }
+
+  /* STUDENTS */
+
+  registerStudent = student => {
+
+      this.firestore.collection('students').add(student)
+      .then(() => {
+          console.log('Deu Certo')
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  } 
+
+  updateStudent = student => {
+      this.firestore.collection('students').doc(student.id).set({
+          name: student.name,
+          code: student.code,
+          course: student.course,
+          phone: student.phone,
+          email: student.email
+      })
+      .then(() => {
+          console.log('Deu Certo')
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  deleteStudent = studentId => {
+      this.firestore.collection('students').doc(studentId).delete()
+      .then(() => {
+          console.log('Deu Certo')
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  getStudent = studentId => {
+      this.firestore.collection('students').doc(studentId).get()
+      .then((student) => {
+          if (student.exists) {
+              return student
+          } else {
+              return null
+          }
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  getStudents = () => {
+      this.firestore.collection('students').get()
+      .then((students) => {
+          if (students.exists) {
+              return students
+          } else {
+              return null
+          }
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  /* RESOURCES */
+
+  registerResource = resource => {
+      this.firestore.collection('resources').add(resource)
+      .then(() => {
+          console.log('Deu Certo')
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  } 
+
+  updateResource = resource => {
+      this.firestore.collection('resources').doc(resource.id).set({
+          name: resource.name,
+          status: resource.code,
+          place: resource.place
+      })
+      .then(() => {
+          console.log('Deu Certo')
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  deleteResource = resourceId => {
+      this.firestore.collection('resources').doc(resourceId).delete()
+      .then(() => {
+          console.log('Deu Certo')
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  getResource = resourceId => {
+      this.firestore.collection('resources').doc(resourceId).get()
+      .then((resource) => {
+          if (resource.exists) {
+              return resource
+          } else {
+              return null
+          }
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  getResources = () => {
+      this.firestore.collection('resources').get()
+      .then((resources) => {
+          if (resources.exists) {
+              return resources
+          } else {
+              return null
+          }
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  /* KEYS */
+
+  registerKey = key => {
+      this.firestore.collection('keys').add(key)
+      .then(() => {
+          console.log('Deu Certo')
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  } 
+
+  updateKey = key => {
+      this.firestore.collection('keys').doc(key.id).set({
+          name: key.name,
+          status: key.status,
+          place: key.place
+      })
+      .then(() => {
+          console.log('Deu Certo')
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  deleteKey = keyId => {
+      this.firestore.collection('keys').doc(keyId).delete()
+      .then(() => {
+          console.log('Deu Certo')
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  getKey = keyId => {
+      this.firestore.collection('keys').doc(keyId).get()
+      .then((key) => {
+          if (key.exists) {
+              return key
+          } else {
+              return null
+          }
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  getKeys = () => {
+      this.firestore.collection('keys').get()
+      .then((keys) => {
+          if (keys.exists) {
+              return keys
+          } else {
+              return null
+          }
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  /* ROOMS */
+
+  registerRoom = room => {
+      this.firestore.collection('rooms').add(room)
+      .then(() => {
+          console.log('Deu Certo')
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  } 
+
+  updateRoom = room => {
+      this.firestore.collection('rooms').doc(room.id).set({
+          name: room.name,
+          status: room.status
+      })
+      .then(() => {
+          console.log('Deu Certo')
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  deleteRoom = roomId => {
+      this.firestore.collection('rooms').doc(roomId).delete()
+      .then(() => {
+          console.log('Deu Certo')
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  getRoom = roomId => {
+      this.firestore.collection('rooms').doc(roomId).get()
+      .then((room) => {
+          if (room.exists) {
+              return room
+          } else {
+              return null
+          }
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  getRooms = () => {
+      this.firestore.collection('rooms').get()
+      .then((rooms) => {
+          if (rooms.exists) {
+              return rooms
+          } else {
+              return null
+          }
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  /* PLACES */
+
+  registerPlace = place => {
+      this.firestore.collection('places').add(place)
+      .then(() => {
+          console.log('Deu Certo')
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  } 
+
+  updatePlace = place => {
+      this.firestore.collection('places').doc(place.id).set({
+          name: place.name
+      })
+      .then(() => {
+          console.log('Deu Certo')
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  deletePlace = placeId => {
+      this.firestore.collection('places').doc(placeId).delete()
+      .then(() => {
+          console.log('Deu Certo')
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  getPlace = placeId => {
+      this.firestore.collection('places').doc(placeId).get()
+      .then((place) => {
+          if (place.exists) {
+              return place
+          } else {
+              return null
+          }
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+
+  getPlaces = () => {
+      this.firestore.collection('places').get()
+      .then((places) => {
+          if (places.exists) {
+              return places
+          } else {
+              return null
+          }
+      })
+      //Não deu certo
+      .catch(() => {
+          console.log('Deu Errado')
+      });
+  }
+}
 
 export default Firebase;
